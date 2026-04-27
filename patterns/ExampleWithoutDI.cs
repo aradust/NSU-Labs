e50306ff502321140710e9fde6b5f65303ba1040
Alexandr Radustov <alexandrradustov@yandex.ru>
2026-04-27 20:36:02 +0700

    add DI pattern

---
+++ patterns/ExampleWithoutDI.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ExampleWithoutDI
+{
+    public class SimpleLogService
+    {
+        public void Write(string message) => Console.WriteLine(message);
+    }
+
+    public class Logger
+    {
+        SimpleLogService simpleLogService = new SimpleLogService();
+
+        public void Log(string message) => simpleLogService.Write($"{DateTime.Now}: {message}");
+
+    }
+}
