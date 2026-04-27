8cbe7470fcd3733090161bd76a5978198da10602
Alexandr Radustov <alexandrradustov@yandex.ru>
2026-04-28 00:08:02 +0700

    add singletone

--- patterns/Program.cs
+++ patterns/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿var a = Singletone.Instance;
+var b = Singletone.Instance;
+
+Console.WriteLine(ReferenceEquals(a, b));
+public class Singletone
+{
+    private static readonly Singletone _instance = new Singletone();
+
+    public static Singletone Instance => _instance;
+    
+    private Singletone()
+    {
+
+    }
+}
+
